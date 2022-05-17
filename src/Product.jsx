fe05e4fe2177221941055e162f48f07f3d329099
gkatsikis <geo.bjj.kat@gmail.com>
2022-05-17 13:46:58 -0400

    add styling to index.css

--- src/Product.jsx
+++ src/Product.jsx
@@ -7,7 +7,10 @@ const Product = (props) => {
       <div className="product">
         <div className="product__info">
           <p>Las Lajas - Costa Rica - SL28</p>
-          <p className="product__price">$36</p>
+          <p className="product__price">
+            <small></small>
+            <strong>36$</strong>
+          </p>
           <p className="product__rating">🌟🌟🌟🌟🌟</p>
         </div>
         <img src="//cdn.shopify.com/s/files/1/1707/3261/products/CostaRicaLasLajas-10oz_1200x.png?v=1648490427" alt="coffee product" />

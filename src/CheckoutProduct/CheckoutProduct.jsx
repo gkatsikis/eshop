f479d3bf174aabc414e3f61cd1a15b1453245121
gkatsikis <geo.bjj.kat@gmail.com>
2022-05-20 16:38:07 -0400

    add html elements for checkoutproduct

--- src/CheckoutProduct/CheckoutProduct.jsx
+++ src/CheckoutProduct/CheckoutProduct.jsx
@@ -3,11 +3,23 @@ import "./CheckoutProduct.css"
 
 const CheckoutProduct = (props) => {
   return ( 
-    <div>
       <div className="checkoutProduct">
         <img src="https://cdn.shopify.com/s/files/1/1707/3261/products/CostaRicaLasLajas-10oz_1200x.png?v=1648490427" alt="" className="checkoutProduct__image" />
+
+        <div className="product__info">
+          <p className="checkoutProduct__title">
+            Onyx - Las Lajas
+          </p>
+          <p className="checkoutProduct__price">
+            <small>$</small>
+            <strong>36</strong>
+          </p>
+          <div className="checkoutProduct__rating">
+            ✡✡✡✡✡ 
+          </div>
+          <button>remove from basket</button>
+        </div>
       </div>
-    </div>
    );
 }
  

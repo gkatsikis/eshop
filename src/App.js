32f7493ae63b739d71aee2da038be10b07c8cc17
gkatsikis <geo.bjj.kat@gmail.com>
2022-05-20 13:02:09 -0400

    add Checkout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+// React
+import React from "react";
+import { ReactDOM } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // Components
 import Header from "./Header"
 import Home from "./Home";
+import Checkout from "./Checkout";
 
 // CSS imports
 import './App.css';
@@ -9,8 +15,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Checkout.css"
+
+const Checkout = (props) => {
+  return ( 
+    <div>
+      <h1>Checkout</h1>
+    </div>
+   );
+}
+ 
+export default Checkout;

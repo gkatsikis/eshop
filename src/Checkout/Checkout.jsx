82fb0bf85f66af4dd1ff2c2f3c9c70ce4878c9a6
gkatsikis <geo.bjj.kat@gmail.com>
2022-05-23 11:35:03 -0400

    map checkout product

--- src/Checkout/Checkout.jsx
+++ src/Checkout/Checkout.jsx
@@ -1,5 +1,6 @@
 // React 
-import React from "react";
+import React, { useState } from "react";
+import { useStateValue } from "../StateProvider/StateProvider"
 
 // Components
 import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
@@ -7,16 +8,26 @@ import Subtotal from "../Subtotal/Subtotal";
 
 // CSS
 import "./Checkout.css"
+import { ListItemAvatar } from "@material-ui/core"
 
 const Checkout = (props) => {
+  const [{basket}, dispatch] = useStateValue()
+
   return ( 
     <div className="checkout">
       <div className="checkout__left">
         <img src="https://i.imgur.com/0bV4A9r.png" alt="checkout add" className="checkout__ad"/>
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-          <CheckoutProduct />
-          <CheckoutProduct />
+          {basket.map(item => (
+            <CheckoutProduct 
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              image={item.image}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
       <div className="checkout__right">

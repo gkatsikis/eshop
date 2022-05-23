cc2e3844c596450c4ab7285d1632b1c422834a3f
gkatsikis <geo.bjj.kat@gmail.com>
2022-05-23 11:40:52 -0400

    individualize items in basket

--- src/CheckoutProduct/CheckoutProduct.jsx
+++ src/CheckoutProduct/CheckoutProduct.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import "./CheckoutProduct.css"
 
 const CheckoutProduct = (props) => {
+
+
   return ( 
       <div className="checkoutProduct">
-        <img src="https://cdn.shopify.com/s/files/1/1707/3261/products/CostaRicaLasLajas-10oz_1200x.png?v=1648490427" alt="" className="checkoutProduct__image" />
+        <img src={props.image} alt="" className="checkoutProduct__image" />
 
         <div className="checkoutProduct__info">
           <p className="checkoutProduct__title">
-            Onyx - Las Lajas
+            {props.title}
           </p>
           <p className="checkoutProduct__price">
             <small>$</small>
-            <strong>36</strong>
+            <strong>{props.price}</strong>
           </p>
           <div className="checkoutProduct__rating">
-            🌟🌟🌟🌟🌟  
+            {Array(props.rating)
+              .fill()
+              .map((_, i) => (
+                <p>🌟</p>
+              ))}  
           </div>
           <button>remove from basket</button>
         </div>
